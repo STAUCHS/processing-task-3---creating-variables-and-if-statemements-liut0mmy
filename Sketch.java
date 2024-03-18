6c8357658426bf76f672027d493ed483d1af160d
liut0mmy <Tommy.Liu25@ycdsbk12.ca>
2024-03-18 14:19:34 -0400

    Random House Locations

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,20 @@
-import processing.core.PApplet;
+import processing.core.PApplet; 
 
 public class Sketch extends PApplet {
-	
+
+  // Declare Variables
+  float rectX;
+  float rectY;
+  float triangleX;
+  float triangleY;
+  float triangleZ;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1000, 1000);
   }
 
   /** 
@@ -16,21 +22,44 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(101, 194, 181);
+
+    // Show current time
+    int hr = hour();
+    int min = minute();
+
+    fill(0);
+    textSize(40);
+    text(hr, 15, 40);
+    text(min, 55, 40);
+
+
+    // House
+    rectX = random(0,1000);
+    rectY = random(0,1000);
+    
+    triangleX = random(0, 1000);
+    triangleY = random(0, 1000);
+    triangleZ = random(0, 1000);
+    
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    // Roof for house
+    fill(191, 101, 101);
+    stroke(0);
+    strokeWeight(5);
+    triangle(triangleX, triangleY, triangleX + 100, triangleY - 50, triangleX + 200, triangleY);
+
+    // House Base
+    fill(196, 149, 134);
+    stroke(0);
+    strokeWeight(3);
+    rect(triangleX, triangleY, 200, 200);
+
+    }
 }
